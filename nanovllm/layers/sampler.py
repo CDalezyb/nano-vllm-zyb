824e994a96249ea388b31a3643eeac2abf9b4b1e
CDalezyb <daleandfire@163.com>
2026-01-15 00:53:05 -0800

    add notes for sampler

--- nanovllm/layers/sampler.py
+++ nanovllm/layers/sampler.py
@@ -9,8 +9,11 @@ class Sampler(nn.Module):
 
     @torch.compile
     def forward(self, logits: torch.Tensor, temperatures: torch.Tensor):
+        # 广播，logits.shape：[bs, vocab_size], temperatures.shape:[bs]
         logits = logits.float().div_(temperatures.unsqueeze(dim=1))
+        # probs: [bs, vocab_size]
         probs = torch.softmax(logits, dim=-1)
+        # argmax 操作之前都是 [bs, vocal_size], 之后变成 [bs]
         sample_tokens = probs.div_(
             torch.empty_like(probs).exponential_(1).clamp_min_(1e-10)
         ).argmax(dim=-1)
