eb969fe4f6135b6d579464a666b5d17456678535
CDalezyb <daleandfire@163.com>
2026-01-26 21:10:13 -0800

    add Qwen3 MoE support but without EP

--- nanovllm/models/supported_models.py
+++ nanovllm/models/supported_models.py
@@ -1,7 +1,9 @@
 from .qwen3 import Qwen3ForCausalLM
 from .qwen2 import Qwen2ForCausalLM
+from .qwen3_moe import Qwen3MoeForCausalLM
 
 model_dict = {
     "qwen3": Qwen3ForCausalLM,
     "qwen2": Qwen2ForCausalLM,
+    "qwen3_moe": Qwen3MoeForCausalLM,
 }
